eef4a475526ac3e093368262c12ffff2034b46ee
grischenkopavel <sXurWmKWTM9M5F2>
2022-10-07 17:16:00 -0400

    RozetkaSimpleTest

--- src/test/java/lesson22/ClickTest.java
+++ src/test/java/lesson22/ClickTest.java
@@ -2,10 +2,52 @@ package lesson22;/*
 Created by Pavel Gryshchenko on 06.10.2022
 */
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class ClickTest {
     private WebDriver driver;
+    private final String ROZETKA_URL = "https://rozetka.com.ua/";
 
+    @BeforeTest
+    void setUpTest()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+    @Test
+    void googleClickTestImplicitlyWait() {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(ROZETKA_URL);
+        driver.findElement(By.xpath("//button[@class = 'header__button ng-star-inserted']")).click();
+
+
+    }
+    @Test
+    void googleClickTest() {
+
+        driver.get(ROZETKA_URL);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until (ExpectedConditions.elementToBeClickable(By.xpath("//button[@class = 'header__button ng-star-inserted']")) );
+
+        element.click();
+
+
+    }
+    @AfterTest
+    void afterTest(){
+        driver.quit();
+    }
 }
--- src/test/java/lesson22/RozetkaSimpleTest.java
+++ src/test/java/lesson22/RozetkaSimpleTest.java
@@ -3,23 +3,54 @@ Created by Pavel Gryshchenko on 06.10.2022
 */
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class RozetkaSimpleTest {
     private WebDriver driver;
+    private final String ROZETKA_URL = "https://rozetka.com.ua/";
+    private final String EXPECTED_TITLE = "Комп'ютер Apple Mac Studio M1 Ultra/48 ядер GPU/64GB/1TB";
+    private WebDriverWait wait;
 
     @BeforeTest
-    public void setUpTest()
+    void setUpTest()
     {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(ROZETKA_URL);
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
     }
     @Test
-    public void RozetkaTest(){
+    void RozetkaTest()
+    {
+        WebElement searchInput = driver.findElement(By.name("search"));
+        searchInput.sendKeys("Mac");
 
+        WebElement btnSearch = driver.findElement(By.xpath("//button[contains(@class, \"button_color_green\")]"));
+        btnSearch.click();
+
+        WebElement firstProduct = wait.
+                until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class=\"goods-tile__title\"]")));
+
+        String firstProductTitle = firstProduct.getText().trim();
+
+        Assert.assertEquals(firstProductTitle, EXPECTED_TITLE, "title doe's not equal to Mac");
+    }
+    @AfterTest
+    void afterTest(){
+        driver.quit();
     }
 }
